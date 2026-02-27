95b8100ea2974850374d1a197889de8b01a281e0
Alyson <alysonlucn@gmail.com>
2026-02-27 19:19:21 -0300

    feat: add home page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Clone do TabNews!</h1>
+      <p>"Work hard in silence, let success be your noise." - Frank Ocean</p>
+    </div>
+  );
+}
